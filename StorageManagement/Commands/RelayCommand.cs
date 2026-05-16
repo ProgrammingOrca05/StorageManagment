a28a462ecaa31088b7727271ebc230ef4fd06541
ProgrammingOrca05 <keyjayningago@gmail.com>
2026-05-16 16:11:11 +0300

    Relay Command setup and PropertyChange

--- StorageManagement/Commands/RelayCommand.cs
+++ StorageManagement/Commands/RelayCommand.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace StorageManagement.Commands
 {
-    internal class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        // WPF calls this to decide if the button should be enabled
+        public bool CanExecute(object parameter)
+        {
+            // If no canExecute was provided, always return true (always enabled)
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        // WPF listens to this event to know when to re-check CanExecute
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    // Generic version — for when you want strong typing (e.g. Action<string>)
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null) return true;
+            return parameter is T t && _canExecute(t);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is T t)
+                _execute(t);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
     }
 }
