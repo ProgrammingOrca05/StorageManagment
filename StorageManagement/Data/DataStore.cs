fecc5eccc5694259d12df1089a4f556c49a99079
ProgrammingOrca05 <keyjayningago@gmail.com>
2026-05-17 03:34:22 +0300

    يجدعان احا نسينا اننا هنحتاج نستخدم ليستات من بعض ف هنحتاج ملف فيه الليستات بتوع الproducts و الtransactions

---
+++ StorageManagement/Data/DataStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageManagement.Data
+{
+    public class DataStore
+    {
+    }
+}
