9af3843f1f07424cd3e4f6f1f199a13c0dc061b6
ProgrammingOrca05 <keyjayningago@gmail.com>
2026-05-30 04:22:23 +0300

    Emergency

--- StorageManagement/ViewModels/ViewModelBase.cs
+++ StorageManagement/ViewModels/ViewModelBase.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace StorageManagement.ViewModels
 {
-    public class ViewModelBase
+    public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
