f96b85edb652979b8051f7f4a4771f2716e36a2a
ProgrammingOrca05 <keyjayningago@gmail.com>
2026-06-14 22:38:07 +0300

    Finished everything!

--- StorageManagement/ViewModels/MainViewModel.cs
+++ StorageManagement/ViewModels/MainViewModel.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using StorageManagement.Commands;
+using StorageManagement.ViewModels;
 
 namespace StorageManagement.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel:ViewModelBase
     {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        // Navigation commands
+        public ICommand ShowInventoryCommand { get; }
+        public ICommand ShowSalesTerminalCommand { get; }
+        public ICommand ShowTransactionHistoryCommand { get; }
+
+        public MainViewModel()
+        {
+            // Initialize child viewmodels
+            var inventoryVM = new InventoryViewModel();
+            var salesVM = new SalesTerminalViewModel();
+            var historyVM = new TransactionHistoryViewModel();
+
+            // Set up navigation commands
+            ShowInventoryCommand = new RelayCommand(o => CurrentView = inventoryVM);
+            ShowSalesTerminalCommand = new RelayCommand(o => CurrentView = salesVM);
+            ShowTransactionHistoryCommand = new RelayCommand(o => CurrentView = historyVM);
+
+            // Default view on login
+            CurrentView = inventoryVM;
+        }
     }
 }
