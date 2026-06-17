78f1f8fa96c56d2f3ca766c5283009c4ecee5e01
ProgrammingOrca05 <keyjayningago@gmail.com>
2026-06-17 15:53:26 +0300

    fixed the id issue

--- StorageManagement/Data/DataStore.cs
+++ StorageManagement/Data/DataStore.cs
@@ -5,13 +5,13 @@ namespace StorageManagement.Data
 {
     public static class DataStore
     {
-       private static int _nextProductId = -5;
+       private static int _nextProductId = 1;
        public static ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
        public static ObservableCollection<Transaction> Transactions { get; set; } = new ObservableCollection<Transaction>();
 
         static DataStore()
         {
-            _nextProductId = 6;
+            _nextProductId = 1;
         }
 
         public static int GetNextProductId() => _nextProductId++;
